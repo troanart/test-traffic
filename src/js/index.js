873a3c1edecb5a2b4360b1499bae4dab4cbce401
Artem Troyan <troanart@gmail.com>
2025-01-30 11:25:14 +0200

    finish all , and fix bags

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 "use strict";
 $(document).ready(function () {
-  // Первый ползунок (1-12 месяцев)
   $("#range").ionRangeSlider({
     skin: "round",
     type: "single",
@@ -14,16 +13,15 @@ $(document).ready(function () {
     values: ["1 month", "3 month", "6 month", "9 month", "12 month"],
   });
 
-  // Второй ползунок (300$ - 15,000$)
   $("#range2").ionRangeSlider({
     skin: "round",
     type: "single",
     min: 300,
     max: 10000,
     from: 777,
-    step: 1, // default 1 (set step)
-    grid: true, // default false (enable grid)
-    grid_num: 4, // default 4 (set number of grid cells)
+    step: 1,
+    grid: true,
+    grid_num: 4,
     grid_snap: false,
     postfix: " €",
   });
@@ -36,4 +34,72 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+});
+
+const input = document.querySelector("#phone");
+const iti = window.intlTelInput(input, {
+  initialCountry: "it",
+  separateDialCode: true,
+  utilsScript:
+    "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
+});
+
+const burger = document.querySelector(".nav__burger");
+const modal = document.querySelector(".modal");
+const nav = document.querySelector(".nav");
+
+burger.addEventListener("click", () => {
+  modal.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.remove("active");
+    nav.classList.remove("active");
+  }
+});
+
+const closeCross = document.querySelector(".modal__close");
+
+closeCross?.addEventListener("click", (e) => {
+  e.stopPropagation();
+  modal.classList.remove("active");
+  nav.classList.remove("active");
+});
+
+document.getElementById("myForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let isValid = true;
+
+  const name = document.getElementById("name");
+  if (name.value.trim().length < 3) {
+    alert("Name must be at least 3 characters long.");
+    isValid = false;
+  }
+
+  const surname = document.getElementById("surname");
+  if (surname.value.trim().length < 3) {
+    alert("Surname must be at least 3 characters long.");
+    isValid = false;
+  }
+
+  const email = document.getElementById("email");
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  if (!email.value.match(emailPattern)) {
+    alert("Please enter a valid email address.");
+    isValid = false;
+  }
+
+  const phone = document.getElementById("phone");
+  const phonePattern = /^\+?[0-9]{1,15}$/;
+  if (!phone.value.match(phonePattern)) {
+    alert("Please enter a valid phone number.");
+    isValid = false;
+  }
+
+  if (isValid) {
+    alert("Form is valid! You can submit now.");
+  }
 });
